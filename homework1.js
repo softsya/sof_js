20655ccc76b88f3186ad511188ad707896d8fc43
softsya <fedoraksofia@gmail.com>
2022-05-29 21:43:57 +0200

    addhomework

---
+++ homework1.js
@@ -0,0 +1,16 @@
+let myName;
+myName = "Sofia";
+
+let myAge = 24;
+myAge = 25;
+
+const myHometown = "Lviv";
+
+let radiohead = ["Thom", "Jonny", "Ed", "Colin", "Phil"];
+
+let me = {
+  name: "sofi",
+  surname: "fedorak",
+  age: 24,
+}
+me.age = 25;
